cc41ed4411ea07febb3e03fd525ea2981e086643
Neil Slater <neil@aframe.com>
2013-09-19 20:49:19 +0100

    Add definition of DBL2NUM for 1.8.7

--- ext/baz/baz_vector_ruby.h
+++ ext/baz/baz_vector_ruby.h
@@ -6,6 +6,11 @@
 #include "baz_vector_lib.h"
 #include <ruby.h>
 
+// Ruby 1.8.7 compatibility patch
+#ifndef DBL2NUM
+#define DBL2NUM( dbl_val ) rb_float_new( dbl_val )
+#endif
+
 void init_baz_vector( VALUE parent_module );
 
 #endif
